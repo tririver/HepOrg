500f94328c5d17574acfced0363f4bda8d4a76ef
Yi Wang <tririverwangyi@gmail.com>
2011-11-25 21:26:15 -0500

    v_0.1_git2: wrote a setup.sh file to help setting up the code, typos fixed

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# modify if want to use other dir as data dir
+LOCALDIR=$(pwd)
+
+# modify if want to use custom pdf reader
+READER=/usr/bin/xdg-open 
+READERARG=
+
+mkdir -p $LOCALDIR/pdf
+mkdir -p $LOCALDIR/org
+echo "pdf_reader = '$READER'" > refconf.py
+echo "pdf_reader_arg = '$READERARG'" >> refconf.py
+echo "dir_prefix = '$LOCALDIR/'" >> refconf.py
+
+# run a test
+echo "Setup finished, running a test..."
+echo ""
+echo "Download a paper may need some time"
+echo "If everything works, you will find your system pdf-reader popup,"
+echo "  and the pdf file is saved to ./pdf/Maldacena_astro-ph_0210603.pdf"
+echo "Also, the meta data is restored in ./org/classic.org"
+echo "It is a text file, but best read by emacs org-mode."
+echo ""
+echo "If there is a problem, have a look at refconf.py"
+echo "Or report bug to tririverwangyi@gmail.com"
+./arxiv_classic.py test_arxiv.htm
+
